94d90703925809b9aa49bb908ca66146da9c7928
go56suk <ayushoffi.diyundi@tum.de>
2024-01-30 07:53:25 +0100

    Inx

--- core/src/de/tum/cit/ase/maze/GameScreen.java
+++ core/src/de/tum/cit/ase/maze/GameScreen.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
